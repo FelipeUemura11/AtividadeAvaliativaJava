834d5e3f5dde198f07bd245fab24f6d0c7603dfb
FelipeYSU <125041064+FelipeUemura11@users.noreply.github.com>
2024-03-29 17:25:44 -0300

    Aividade 6 - Contato

---
+++ Contato.java
@@ -0,0 +1,19 @@
+public class Contato {
+    private String nome;
+    private String telefone;
+
+    public Contato(String nome, String telefone){
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public String getTelefone(){
+        return this.telefone;
+    }
+
+
+}
